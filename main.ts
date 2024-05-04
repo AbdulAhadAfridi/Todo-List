c9e66c197c25960ae6331c9236eb31bf8b99a7bd
AbdulAhadAfridi <160354801+AbdulAhadAfridi@users.noreply.github.com>
2024-05-04 07:46:28 -0700

    First coomit

---
+++ main.ts
@@ -0,0 +1,27 @@
+#! usr/bin/env node
+import inquirer from "inquirer"
+
+let todo = [];
+let condition = true;
+while (condition ){
+
+
+let addTask = await inquirer.prompt(
+    [
+        {
+            name:"todos",  
+            type:"input",
+            message:"what you want to add in your todos?"
+        },
+        {
+            name:"addMore",
+            type:"confirm",
+            message:"Do you want to add more?",
+            default:"false"
+        }
+    ]
+);
+todo.push(addTask.todos);
+condition = addTask.addMore
+console.log(todo);
+}
